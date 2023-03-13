c66d2baccc56aadcb2f03444d0035aacc7c2469c
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-13 14:24:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = 'Welcome to Flatbook, ';
+
+welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER!';
+
+let shortGreeting = 'Welcome, ';
+
+shortGreeting = 'Welcome, G!';
